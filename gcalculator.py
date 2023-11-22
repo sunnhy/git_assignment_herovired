4fb11ff65b93b663c34f6d4fbe43d2c4bb440fbd
Newton B <newtonb@Newtons-MacBook-Air.local>
2023-11-22 09:43:40 +0530

    new 001

--- gcalculator.py
+++ gcalculator.py
@@ -9,16 +9,4 @@ class GeometryCalculator:
     def calculate_rectangle_area(self, length, width):
 
         return length * width
-
-if __name__ == "__main__":
-
-    calculator = GeometryCalculator()
-
-    radius = 5
-    length = 10
-    width = 6
-
-print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
-
-print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
-
+    
